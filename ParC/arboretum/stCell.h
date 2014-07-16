a25f69f17be6e7bfb54ba2b95ed66c5cc15d03aa
Antonio Canabrava Fraideinberze <acfraideinberze@gmail.com>
2014-07-15 23:51:32 -0400

    First few changes in the project. Added the files from bow and changed some properties.

---
+++ ParC/arboretum/stCell.h
@@ -0,0 +1,45 @@
+#ifndef __STCELL_H
+#define __STCELL_H
+
+#include "stCellId.h"
+#include "stNode.h"
+
+class stNode; //informs that the class exists
+
+class stCell {
+   public:
+      stCell(int dimensionality) {
+		 sumOfPoints = 0;
+		 nextLevel = 0;
+		 id = 0;
+      }
+
+      ~stCell() {
+		 if (nextLevel) {
+			//delete nextLevel;
+		 }
+      }
+
+	  void insertPoint() {
+	     sumOfPoints++;
+	  }
+
+	  int getSumOfPoints() {
+	     return sumOfPoints;
+	  }
+
+	  stCellId *getId() {
+	     return id;
+	  }
+
+	  void setId(stCellId *id) {
+	     this->id = id;
+	  }
+
+      stNode *nextLevel;
+   private:
+      int sumOfPoints;
+	  stCellId *id;
+};
+
+#endif //__STCELL_H
