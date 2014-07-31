47bd4351d57486e98159adbfb71725be66d22f83
Antonio Canabrava Fraideinberze <acfraideinberze@gmail.com>
2014-07-31 08:29:38 -0400

    Added two java classes. Need to make it better.

--- ParC/mapper.cpp
+++ ParC/mapper.cpp
@@ -6,48 +6,63 @@
 
 using namespace std;
 
+#ifndef __GNUG__
+    #include "stFDR.h"
+#endif //__GNUG__
+
+#ifdef __GNUG__
+    #include "stFDR.cpp"
+#endif //__GNUG__
+
 int main(int argc, char **argv) {
-   char point [10000]; // one point
-   char num [100]; // one value
-   int dim = 0; // data dimensionality
-   long s = 0; // data size
-   int div = 0; // desired number of data divisions
-   long pointId = 0; // point identifier
+    char point[10000]; // one point
+    char num[100]; // one value
+    int dim = 0; // data dimensionality
+    long s = 0; // data size
+    int div = 0; // desired number of data divisions
+    long pointId = 0; // point identifier
 
-   // reads the data dimensionality
-   FILE *dimensionality;
-   dimensionality=fopen("dimensionality", "r");
-   fscanf(dimensionality,"%d",&dim);
-   fclose(dimensionality);
+    // reads the data dimensionality
+    FILE * dimensionality;
+    dimensionality = fopen("dimensionality", "r");
+    fscanf(dimensionality, "%d", &dim);
+    fclose(dimensionality);
 
-   // reads the data size
-   FILE *size;
-   size=fopen("size", "r");
-   fscanf(size,"%ld",&s);
-   fclose(size);
+    // reads the data size
+    FILE * size;
+    size = fopen("size", "r");
+    fscanf(size, "%ld", &s);
+    fclose(size);
 
-   // reads the desired number of data divisions
-   FILE *divisions;
-   divisions=fopen("divisions", "r");
-   fscanf(divisions,"%d",&div);
-   fclose(divisions);
+    // reads the desired number of data divisions
+    FILE * divisions;
+    divisions = fopen("divisions", "r");
+    fscanf(divisions, "%d", &div);
+    fclose(divisions);
 
-   while(cin) { // reads one line per iteration
-      cin >> num; // point identifier
-      pointId = atol(num);
+    int numberOfObjects = 0;
 
-      cin >> point; // first value
-      for (int i=1; i<dim; i++) {
-   		cin >> num; // other values
-        strcat(point,"_");
-		strcat(point,num);
-      }
-      cin >> num; // discarts the ground truth
-      if (cin) { //  outputs the point with the appropriate key
-        sprintf(num,"%d",((int)floor(pointId/ceil((double)(s+1)/div)))); // file position based data division
-		cout << num << "\t" << point << "\n";
-      }
-   }
 
-   return 0; // success
+    while(cin) { // reads one line per iteration
+        cin >> num; // point identifier
+        pointId = atol(num);
+
+        cin >> point; // first value
+        for(int i = 1; i < dim; i++) {
+            cin >> num; // other values
+            strcat(point, "_");
+            strcat(point, num);
+        }
+
+        cin >> num; // discarts the ground truth
+        if(cin) { //  outputs the point with the appropriate key
+             // file position based data division
+            sprintf(num, "%d", ((int) floor(pointId / ceil((double) (s + 1) / div))));
+            cout << num << "\t" << point << "\n";
+        }
+
+        printf("User: %s - %s\n", num, point);
+    }
+
+    return 0; // success
 }
